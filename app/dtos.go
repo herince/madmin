3992d34a4f101f27b539e5a705797fd8cf650c8b
herince <rherince@gmail.com>
2017-02-13 04:00:59 +0200

    fix initial godoc

--- app/dtos.go
+++ app/dtos.go
@@ -1,14 +1,12 @@
 package app
 
 // JSON format for the GET requests for "/<items>/"
-
 type CollectionResponseDTO struct {
 	Info string   `json:"info"`
 	Urls []string `json:"urls"`
 }
 
 // JSON format for the GET requests for "/<items>/<id>"
-
 type StockDTO struct {
 	Id string `json:"id"`
 
@@ -23,7 +21,6 @@ type StockDTO struct {
 
 // New stock items' JSON format
 // IDs of new stock objects should be handled only by the back-end
-
 type NewStockDTO struct {
 	Name string    `json:"name"`
 	Type stockType `json:"type"`
