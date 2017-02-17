e8b7f7dd0566e8f84109634df964e0586fc4f7b0
herince <rherince@gmail.com>
2017-02-17 18:03:59 +0200

    add doc.go file with package info

---
+++ app/doc.go
@@ -0,0 +1,3 @@
+// Package app implements the back-end logic for the vet pharmacy administrating system.
+// It implements a simple RESTful API that manages somehow the vet pharmacy warehouse.
+package app
